dd18cb8fbcaad8fe570bce0794d4faeac393459b
rex <905578725@qq.com>
2022-02-08 23:34:18 +0800

    引入echarts图表

--- src/Pages/s3_sensor_detail.vue
+++ src/Pages/s3_sensor_detail.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <div id="main" style="width: 600px;height:400px;"></div>
 </template>
 
 <script>
+import * as echarts from 'echarts';
+// 基于准备好的dom，初始化echarts实例
+
 export default {
-  name: "s3_sensor_detail"
+  name: "s3_sensor_detail",
+  mounted() {
+    var myChart = echarts.init(document.getElementById('main'));
+    myChart.setOption({
+      title: {
+        text: 'ECG设备'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['2月1日', '2月2日', '2月3日', '2月4日', '2月5日']
+      },
+      yAxis: {
+      },
+      series: [
+        {
+          name: '活跃设备数',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10]
+        }
+      ]
+    });
+  }
 }
 </script>
 
