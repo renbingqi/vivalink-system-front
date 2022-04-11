613e0000905720171788ea7f29f0cb78b59a7476
rex <905578725@qq.com>
2022-04-12 00:10:59 +0800

    添加API UI制作ing

--- src/Pages/test/apiTest.vue
+++ src/Pages/test/apiTest.vue
@@ -1,5 +1,64 @@
 <template>
   <div id="body">
+    <el-dialog title="新增API" :visible.sync="dialogFormVisible" width="95%" top="1vh">
+      <div style="height: 500px;">
+        <div class="header">
+          <div class="Method">
+            <el-select v-model="value" placeholder="Method">
+              <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+          <div class="url">
+            <el-input></el-input>
+          </div>
+          <div class="send">
+            <el-button type="primary" round>Send</el-button>
+          </div>
+        </div>
+        <div class="header_type">
+          <el-tabs v-model="activeName" @tab-click="handleClick_tag">
+            <el-tab-pane label="Params" name="first">
+              <div>
+                <el-table
+                    :data="tableData_Params"
+                    border
+                    style="width: 100%"
+                height="150px">
+                  <el-table-column
+                      prop="key"
+                      label="KEY"
+                      width="280">
+                  </el-table-column>
+                  <el-table-column
+                      prop="value"
+                      label="VALUE"
+                      width="280">
+                  </el-table-column>
+                  <el-table-column
+                      prop="description"
+                      label="DESCRIPTION"
+                      width="280">
+                  </el-table-column>
+                </el-table>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="Headers" name="second">Headers</el-tab-pane>
+            <el-tab-pane label="Body" name="third">Body</el-tab-pane>
+          </el-tabs>
+        </div>
+
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+
     <div id="left-side">
       <div class="header">
         <div class="search">
@@ -19,7 +78,7 @@
                 label="项目列表"
                 align="center">
               <template slot-scope="scope">
-                <el-button  plain class="project-button" @click="handleClick(scope)"> {{scope.row}}</el-button>
+                <el-button plain class="project-button" @click="handleClick(scope)"> {{ scope.row }}</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -71,7 +130,7 @@
             label="操作"
             show-overflow-tooltip
             align="center"
-        width="200px">
+            width="200px">
           <template slot-scope="scope">
             <el-button
                 size="mini"
@@ -97,11 +156,14 @@
         <el-button type="primary"
                    size="mini"
                    icon="el-icon-circle-plus-outline"
-                   style="margin-left: 480px">新增</el-button>
+                   style="margin-left: 480px"
+                   @click="dialogFormVisible = true">新增
+        </el-button>
         <el-button type="danger"
                    size="mini"
                    icon="el-icon-delete"
-        style="margin-left: 20px">删除选中项</el-button>
+                   style="margin-left: 20px">删除选中项
+        </el-button>
       </div>
     </div>
   </div>
@@ -110,82 +172,115 @@
 <script>
 export default {
   name: "apiTest",
-  data(){
+  data() {
     return {
       tableData_api: [{
         id: 1,
         name: 'vCloud Data Query ',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/01"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/01"
+      }, {
         id: 2,
         name: 'Analysis Service',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/03"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/03"
+      }, {
         id: 3,
         name: 'Server SDK',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/05"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/05"
+      }, {
         id: 1,
         name: 'vCloud Data Query',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/01"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/01"
+      }, {
         id: 2,
         name: 'Analysis Service',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/03"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/03"
+      }, {
         id: 3,
         name: 'Server SDK',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/05"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/05"
+      }, {
         id: 1,
         name: 'vCloud Data Query',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/01"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/01"
+      }, {
         id: 2,
         name: 'Analysis Service',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/03"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/03"
+      }, {
         id: 3,
         name: 'Server SDK',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/05"
-      },{
+        createTime: "2022/04/01",
+        updateTime: "2022/04/05"
+      }, {
         id: 1,
         name: 'vCloud Data Query',
         module: "Get Token",
-        createTime:"2022/04/01",
-        updateTime:"2022/04/01"
+        createTime: "2022/04/01",
+        updateTime: "2022/04/01"
       }],
-      tableData:["vcloud","vcloud2","vcloud3","vcloud4","vcloud5","vcloud6","vcloud7","vcloud5","vcloud6","vcloud7"]
+      tableData: ["vcloud", "vcloud2", "vcloud3", "vcloud4", "vcloud5", "vcloud6", "vcloud7", "vcloud5", "vcloud6", "vcloud7"],
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      formLabelWidth: '120px',
+      options: [
+        {
+          value: '选项1',
+          label: 'GET'
+        },
+        {
+          value: '选项2',
+          label: 'POST'
+        },
+
+      ],
+      activeName: 'second',
+      tableData_Params: [{
+        key: '2016-05-02',
+        value: '王小虎',
+        description: '上海市普陀区金沙江路 1518 弄'}]
+
     }
   },
-  methods:{
-    handleClick(scope){
+  methods: {
+    handleClick(scope) {
       console.log(scope)
+    },
+    handleClick_tag(tab, event) {
+      console.log(tab, event);
     }
   }
 }
 </script>
 
 <style scoped>
-#body{
+#body {
   position: relative;
 }
 
@@ -212,10 +307,12 @@ export default {
   margin-top: 15px;
   /*margin-left: 15px;*/
 }
-.header,.project-list{
+
+.header, .project-list {
   position: absolute;
   width: 100%;
 }
+
 .search-button {
   position: absolute;
   width: 30%;
@@ -224,15 +321,45 @@ export default {
   /*margin-right: 10px;*/
 }
 
-.project-list{
+.project-list {
   position: absolute;
   top: 10%;
   width: 100%;
 }
-.add_item{
+
+.add_item {
   width: 69px;
 }
-.project-button{
+
+.project-button {
   width: 100%;
+}
+
+
+.url, .Method, .send {
+  position: absolute;
+}
+
+.Method {
+  width: 100px;
+  left: 200px;
+}
+
+.url {
+  width: 600px;
+  left: 300px;
+}
+
+.send {
+  left: 950px;
+}
+
+.header_type, .header {
+  position: absolute;
+}
+
+.header_type {
+  top: 120px;
+  left:220px
 }
 </style>
